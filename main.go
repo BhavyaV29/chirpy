f8b4bd9267ae8224ad0da9718dbce9689f20abd0
Bhavya Vashisht <118844921+BhavyaV29@users.noreply.github.com>
2025-05-05 12:51:18 +0000

    Various custom Handers added for file server hit counts and server readiness checking

--- main.go
+++ main.go
@@ -3,21 +3,62 @@ package main
 import (
 	"net/http"
 	"fmt"
+	"sync/atomic"
 )
+type apiConfig struct{
+	fileserverHits atomic.Int32
+}
+
+func readinessHandler(w http.ResponseWriter,r *http.Request){
+	w.Header().Set("Content-Type","text/plain; charset=utf-8")
+	
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) hitCountHandler(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type","text/plain; charset=utf-8")
+	body:= fmt.Sprintf("Hits: %d",cfg.fileserverHits.Load())
+	w.Write([]byte(body))
+}
+
+func (cfg *apiConfig) resetHitHandler(w http.ResponseWriter, r *http.Request){
+	cfg.fileserverHits.Store(0)
+	w.Write([]byte("reset\n"))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler{
+	newNext:= http.HandlerFunc(func (w http.ResponseWriter, r *http.Request){
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w,r)
+	})
+	return newNext
+}
+
+
 func main(){
+	//making our server hit counter
+	
+	apiCfg:=&apiConfig{}
+	
 	//making router
 	mux := http.NewServeMux()
 
-	//defining handler
-	mux.Handle("/",http.FileServer(http.Dir(".")))
-	
+	//defining handlers
+	fileserver:=http.FileServer(http.Dir("."))
+	appHandler:=http.StripPrefix("/app",fileserver) //stripping path
+	mux.Handle("/app/",apiCfg.middlewareMetricsInc(appHandler))
+	mux.HandleFunc("/healthz",readinessHandler)
+	mux.Handle("/metrics",http.HandlerFunc(apiCfg.hitCountHandler))
+	mux.Handle("/reset",http.HandlerFunc(apiCfg.resetHitHandler))
+
 	//setting up server
 	server:= &http.Server{
 		Handler:mux,
 		Addr:":8080",
 	}
 
-
+	//running the server
 	err:=server.ListenAndServe()
 	if err!= nil{
 		fmt.Println(err)
